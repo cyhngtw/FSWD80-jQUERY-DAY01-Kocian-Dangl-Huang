9f1177f2c1a6169d3ce9844031928adfbea36a19
chriswizz <christian.dangl1@gmail.com>
2019-10-07 14:37:12 +0200

    add headers to table

--- grocery/grocery.js
+++ grocery/grocery.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	$("body").append("<tr><th>Name</th><th>Quantity</th><th>Price in €</th><th>Image</th></tr>");
+
 	for (var i = 0; i < grocery.length; i++) {
 		$("body").append("<tr><td>" + grocery[i].name + "</td><td>" + grocery[i].qty + "</td><td>" + grocery[i].price + "</td><td><img src='" + grocery[i].image + "'></td></tr>");
 	}
