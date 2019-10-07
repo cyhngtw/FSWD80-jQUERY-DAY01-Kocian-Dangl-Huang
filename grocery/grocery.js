86a87748ba451d2e804cdf6c067bcd08403884fd
chriswizz <christian.dangl1@gmail.com>
2019-10-07 15:37:38 +0200

    add css with jquery for table

--- grocery/grocery.js
+++ grocery/grocery.js
@@ -3,8 +3,12 @@ $(document).ready(function(){
 	$("body").append("<tr><th>Name</th><th>Quantity</th><th>Price in €</th><th>Image</th></tr>");
 
 	for (var i = 0; i < grocery.length; i++) {
-		$("body").append("<tr><td>" + grocery[i].name + "</td><td>" + grocery[i].qty + "</td><td>" + grocery[i].price + "</td><td><img src='" + grocery[i].image + "'></td></tr>");
+		$("body").append("<tr id='row-"+ [i] +"'><td>" + grocery[i].name + "</td><td>" + grocery[i].qty + "</td><td>" + grocery[i].price + "</td><td><img src='" + grocery[i].image + "'></td></tr>");
+
+		var rowId = "row-" + [i];
+		
+		if (grocery[i].qty < 10) {
+			$("#" + rowId + " :nth-child(2)").css({"color":"blue","font-size":"40px"});
+		}
 	}
-
-
 });
